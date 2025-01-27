70177892831763c0ed530071c4fbc0f86945d207
invincibleAntares <kumarabhishek2928@gmail.com>
2025-01-27 21:37:02 +0530

    starting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
- 
+  const fetchapi = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api");
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching API:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchapi();
+  }, []);
+
   return (
     <>
       <h1>Hello</h1>
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+ 
+ const corsOptions = {
+    origin: 'http://localhost:5173',
+ };
+
+app.use(cors(corsOptions));
+app.get('/api', (req, res) => {
+   res.send('Hello World');
+}) 
+
+app.listen(3000, () => {
+    console.log('Server is running on 3000');
+})
